9b144ff60d1890ef478c92457d3cfcc672c69f98
Adam Adam <adam.haouzi31@gmail.com>
2022-12-20 10:32:47 +0100

    debut de page acceuil avec navbar fini

--- VaBucherFront/src/app/app-routing.module.ts
+++ VaBucherFront/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccueilComponent} from "./components/accueil/accueil.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AccueilComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
