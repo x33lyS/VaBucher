0b8bf49d7b0bba9d40729605c7594471c75d5ef0
FLEJOSNE <florian.lejosne@ynov.com>
2022-12-20 11:40:26 +0100

    [ADD] navbar

--- VaBucherFront/src/app/app-routing.module.ts
+++ VaBucherFront/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AccueilComponent} from "./components/accueil/accueil.component";
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: AccueilComponent
+    component: HomeComponent
   },
 ];
 
--- VaBucherFront/src/app/app.module.ts
+++ VaBucherFront/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 import { FormsModule } from '@angular/forms';
 import { JobofferComponent } from './components/joboffer/joboffer.component';
-import { AccueilComponent } from './components/accueil/accueil.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
@@ -16,7 +16,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     AppComponent,
     AdminPanelComponent,
     JobofferComponent,
-    AccueilComponent,
+    HomeComponent,
     LoaderComponent,
     NavbarComponent
   ],
