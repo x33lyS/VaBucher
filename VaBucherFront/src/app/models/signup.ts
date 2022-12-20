4a6966892e44a854ff399dd92aa2cda7e07b8c12
FLEJOSNE <florian.lejosne@ynov.com>
2022-12-20 16:14:46 +0100

    [ADD] Signup

---
+++ VaBucherFront/src/app/models/signup.ts
@@ -0,0 +1,22 @@
+export class SignupForm {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    search: string;
+    phone: string;
+    location: string;
+  
+    constructor() {
+      this.firstName = '';
+      this.lastName = '';
+      this.email = '';
+      this.password = '';
+      this.confirmPassword = '';
+      this.search = '';
+      this.phone = '';
+      this.location = '';
+    }
+  }
+  
