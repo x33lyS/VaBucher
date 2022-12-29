272a5a8a522c37cb4a04f490fbb41b57037f2d51
Florian Lejosne <florian.lejosne@ynov.com>
2022-12-29 12:09:05 +0100

    [EDIT] rename de signup en registration et ajout des istouched

--- VaBucherFront/src/app/components/registration/signup.component.spec.ts
+++ VaBucherFront/src/app/components/registration/signup.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignupComponent } from './signup.component';
+import { RegistrationComponent } from './registration.component';
 
-describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
+describe('RegistrationComponent', () => {
+  let component: RegistrationComponent;
+  let fixture: ComponentFixture<RegistrationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      declarations: [ RegistrationComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SignupComponent);
+    fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
