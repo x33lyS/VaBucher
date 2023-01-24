bfd3b59e34ac509ef6238c6643f150e3d1b92add
Adam-Hzi <adam.haouzi31@gmail.com>
2023-01-24 13:50:34 +0100

    api call success

--- VaBucherFront/src/app/components/dashboard/dashboard.component.ts
+++ VaBucherFront/src/app/components/dashboard/dashboard.component.ts
@@ -7,14 +7,16 @@ import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  userInput = 'developer';
-  jobs = [];
+  private apiUrl = 'https://api.pole-emploi.io/partenaire/offresdemploi/v2/offres/search?qualification=0&motsCles=informatique&commune=51069,76322,46083,12172,28117&origineOffre=2';
+  private token = 'e85tZgTULngSuHk_THFmQI9-rXo';
 
   constructor(private http: HttpClient) {}
 
-  searchJobs(query: string) {
-    this.http.get(`https://openjobsearch.org/api/search?q=${query}`).subscribe(data => {
-      console.log(data);
+  getOffresEmploi() {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + this.token,
     });
+
+    this.http.get(`${this.apiUrl}`, { headers }).subscribe(data => console.log(data));
   }
 }
