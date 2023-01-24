f523f6642049e341ae3e605da8f98905c066392b
Adam-Hzi <adam.haouzi31@gmail.com>
2023-01-24 08:34:15 +0100

    start api

--- VaBucherFront/src/app/components/dashboard/dashboard.component.ts
+++ VaBucherFront/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  userInput = 'developer';
+  jobs = [];
 
+  constructor(private http: HttpClient) {}
+
+  searchJobs(query: string) {
+    this.http.get(`https://openjobsearch.org/api/search?q=${query}`).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
