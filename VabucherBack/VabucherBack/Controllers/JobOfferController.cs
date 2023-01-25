5ac2720e7c4b8e55c7a59a4f796639ba30e223db
FLEJOSNE <florian.lejosne@ynov.com>
2023-01-25 09:50:45 +0100

    [ADD] crawler try catch update

--- VabucherBack/VabucherBack/Controllers/JobOfferController.cs
+++ VabucherBack/VabucherBack/Controllers/JobOfferController.cs
@@ -38,164 +38,178 @@ namespace VabucherBack.Controllers
         [HttpPost]
         public async Task<ActionResult<List<JobOffer>>> CreateJobOffer(JobOffer jobOffer)
         {
-            
-            try
+            var working = false;
+            while (!working)
             {
                 using (var driver = new ChromeDriver("."))
                 {
-                    //Navigate to DotNet website
-                    driver.Navigate().GoToUrl("https://www.monster.fr/emploi/");
-                    //Click the Get Started button
-                    var submitButton = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
-                    submitButton.Click();
-                    var input = driver.FindElement(By.Id("search-job"));
-                    input.SendKeys(jobOffer.Domain+" etudiant");
-                    submitButton = driver.FindElement(By.ClassName("btn-purple-fill"));
-                    submitButton.Click();
-                    string principalHandle = driver.CurrentWindowHandle;
-                    // Get Started section is a multi-step wizard
-                    // The following sections will find the visible next step button until there's no next step button left
-                    int i = 0;
-                    IWebElement nextLink = null;
-                    var exist = "";
-                    while (nextLink == null)
+                    try
                     {
-                        try
+                        //Navigate to DotNet website
+                        driver.Navigate().GoToUrl("https://www.monster.fr/emploi/");
+                        //Click the Get Started button
+                        var submitButton = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
+                        submitButton.Click();
+                        var input = driver.FindElement(By.Id("search-job"));
+                        input.SendKeys(jobOffer.Domain + " etudiant");
+                        submitButton = driver.FindElement(By.ClassName("btn-purple-fill"));
+                        submitButton.Click();
+                        string principalHandle = driver.CurrentWindowHandle;
+                        // Get Started section is a multi-step wizard
+                        // The following sections will find the visible next step button until there's no next step button left
+                        int i = 0;
+                        IWebElement nextLink = null;
+                        var exist = "";
+                        while (nextLink == null)
                         {
-                            await Task.Delay(5000);
+                            try
+                            {
+                                await Task.Delay(5000);
 
-                            nextLink = driver.FindElement(By.ClassName("job-cardstyle__JobCardComponent-sc-1mbmxes-0"));
-                            exist = nextLink.Text;
+                                nextLink = driver.FindElement(By.ClassName("job-cardstyle__JobCardComponent-sc-1mbmxes-0"));
+                                exist = nextLink.Text;
 
+                            }
+                            catch
+                            {
+                                try
+                                {
+                                    var notfound = driver.FindElement(By.XPath("//h3[@data-testid='messageTitle']"));
+                                    driver.Quit();
+                                    return BadRequest("No Job found.");
+
+                                }
+                                catch { }
+                            }
                         }
-                        catch {
+                        if (exist != "")
+                        {
+                            while (i <= driver.FindElements(By.ClassName("job-cardstyle__JobCardComponent-sc-1mbmxes-0")).Count())
+                            {
+
+
+                                nextLink = driver.FindElements(By.ClassName("job-cardstyle__JobCardComponent-sc-1mbmxes-0")).ElementAt(i);
+
+                                var originalHandles = driver.WindowHandles;
+                                nextLink.Click();
+                                var newHandle = driver.WindowHandles.Except(originalHandles).Single();
+                                driver.SwitchTo().Window(newHandle);
+
+
+
+
+                                JobOffer offer = new JobOffer();
+                                //Title
+                                try
+                                {
+                                    var titleElement = driver.FindElement(By.ClassName("JobViewTitle"));
+                                    offer.Title = titleElement.Text;
+                                    offer.Domain = jobOffer.Domain;
+                                }
+                                catch
+                                {
+                                    offer.Title = "";
+                                }
+                                //CompanyInfo
+                                try
+                                {
+                                    var companyInfoElement = driver.FindElement(By.ClassName("headerstyle__JobViewHeaderCompany-sc-1ijq9nh-6"));
+                                    offer.CompanyInfo = companyInfoElement.Text;
+                                }
+                                catch
+                                {
+                                    offer.CompanyInfo = "";
+                                }
+
+                                //Salary
+                                try
+                                {
+                                    var salaryElement = driver.FindElement(By.XPath("//div[@data-test-id='svx_salaryComponent_body']"));
+                                    offer.Salaire = salaryElement.Text;
+                                }
+                                catch
+                                {
+                                    offer.Salaire = "";
+                                }
+                                //JobTypes
+                                try
+                                {
+                                    var jobTypesElement = driver.FindElement(By.XPath("//div[@data-test-id='svx-jobview-employmenttype']"));
+                                    offer.Types = jobTypesElement.Text;
+                                }
+                                catch
+                                {
+                                    offer.Types = "";
+                                }
+                                //Localisation
+                                try
+                                {
+                                    var localisationElement = driver.FindElement(By.XPath("//div[@data-test-id='svx-jobview-location']"));
+                                    offer.Localisation = localisationElement.Text;
+                                }
+                                catch
+                                {
+                                    offer.Localisation = "";
+                                }
+                                //IsNew
+                                try
+                                {
+                                    var isNewElement = driver.FindElement(By.XPath("//div[@data-test-id='svx-jobview-posted']"));
+                                    offer.IsNew = isNewElement.Text;
+                                }
+                                catch
+                                {
+                                    offer.IsNew = "";
+                                }
+                                //Description
+                                try
+                                {
+                                    var descriptionElement = driver.FindElement(By.ClassName("descriptionstyles__DescriptionBody-sc-13ve12b-4"));
+                                    offer.Description = descriptionElement.Text;
+                                }
+                                catch
+                                {
+                                    offer.Description = "";
+                                }
+                                if (principalHandle != newHandle)
+                                {
+                                    driver.Close();
+                                }
+                                if (!_context.JobOffers.Any(j => j.Title == offer.Title) && offer.Title != "")
+                                {
+                                    _context.JobOffers.Add(offer);
+                                    await _context.SaveChangesAsync();
+                                }
+                                i++;
+                                working = true;
+                                driver.SwitchTo().Window(principalHandle);
+
+                            }
+                            driver.Quit();
                         }
                     }
-                    if (exist != "")
+                    catch
                     {
-                        while (i < driver.FindElements(By.ClassName("job-cardstyle__JobCardComponent-sc-1mbmxes-0")).Count())
-                        {
-
-
-                            nextLink = driver.FindElements(By.ClassName("job-cardstyle__JobCardComponent-sc-1mbmxes-0")).ElementAt(i);
-
-                            var originalHandles = driver.WindowHandles;
-                            nextLink.Click();
-                            var newHandle = driver.WindowHandles.Except(originalHandles).Single();
-                            driver.SwitchTo().Window(newHandle);
-
-
-
-
-                            JobOffer offer = new JobOffer();
-                            //Title
-                            try
-                            {
-                                var titleElement = driver.FindElement(By.ClassName("JobViewTitle"));
-                                offer.Title = titleElement.Text;
-                                offer.Domain = jobOffer.Domain;
-                            }
-                            catch
-                            {
-                                offer.Title = "";
-                            }
-                            //CompanyInfo
-                            try
-                            {
-                                var companyInfoElement = driver.FindElement(By.ClassName("headerstyle__JobViewHeaderCompany-sc-1ijq9nh-6"));
-                                offer.CompanyInfo = companyInfoElement.Text;
-                            }
-                            catch
-                            {
-                                offer.CompanyInfo = "";
-                            }
-
-                            //Salary
-                            try
-                            {
-                                var salaryElement = driver.FindElement(By.XPath("//div[@data-test-id='svx_salaryComponent_body']"));
-                                offer.Salaire = salaryElement.Text;
-                            }
-                            catch
-                            {
-                                offer.Salaire = "";
-                            }
-                            //JobTypes
-                            try
-                            {
-                                var jobTypesElement = driver.FindElement(By.XPath("//div[@data-test-id='svx-jobview-employmenttype']"));
-                                offer.Types = jobTypesElement.Text;
-                            }
-                            catch
-                            {
-                                offer.Types = "";
-                            }
-                            //Localisation
-                            try
-                            {
-                                var localisationElement = driver.FindElement(By.XPath("//div[@data-test-id='svx-jobview-location']"));
-                                offer.Localisation = localisationElement.Text;
-                            }
-                            catch
-                            {
-                                offer.Localisation = "";
-                            }
-                            //IsNew
-                            try
-                            {
-                                var isNewElement = driver.FindElement(By.XPath("//div[@data-test-id='svx-jobview-posted']"));
-                                offer.IsNew = isNewElement.Text;
-                            }
-                            catch
-                            {
-                                offer.IsNew = "";
-                            }
-                            //Description
-                            try
-                            {
-                                var descriptionElement = driver.FindElement(By.ClassName("descriptionstyles__DescriptionBody-sc-13ve12b-4"));
-                                offer.Description = descriptionElement.Text;
-                            }
-                            catch
-                            {
-                                offer.Description = "";
-                            }
-                            if (principalHandle != newHandle)
-                            {
-                                driver.Close();
-                            }
-                            if (!_context.JobOffers.Any(j => j.Title == offer.Title) && offer.Title != "")
-                            {
-                                _context.JobOffers.Add(offer);
-                                await _context.SaveChangesAsync();
-                            }
-                            i++;
-                            driver.SwitchTo().Window(principalHandle);
-
-                        }
+                        driver.Quit();
                     }
 
-                    driver.Quit();
                 }
-            }
-            catch
-            {
 
             }
             return Ok(await _context.JobOffers.ToListAsync());
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<List<JobOffer>>> DeleteJobOffer(int id)
-        {
-            var dbJobOffer = await _context.JobOffers.FindAsync(id);
-            if (dbJobOffer == null)
-                return BadRequest("User not found.");
-
-            _context.JobOffers.Remove(dbJobOffer);
-            await _context.SaveChangesAsync();
-
-            return Ok(await _context.JobOffers.ToListAsync());
-        }
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<List<JobOffer>>> DeleteJobOffer(int id)
+    {
+        var dbJobOffer = await _context.JobOffers.FindAsync(id);
+        if (dbJobOffer == null)
+            return BadRequest("User not found.");
+
+        _context.JobOffers.Remove(dbJobOffer);
+        await _context.SaveChangesAsync();
+
+        return Ok(await _context.JobOffers.ToListAsync());
+    }
+}
 }
