d8da30795d403ced7ade0fc7d33dba28e6655420
Adam Adam <adam.haouzi31@gmail.com>
2023-01-26 19:12:39 +0100

    format api

--- VaBucherFront/src/app/components/joboffer/joboffer.component.ts
+++ VaBucherFront/src/app/components/joboffer/joboffer.component.ts
@@ -4,6 +4,7 @@ import { EventEmitter, Input, Output } from '@angular/core';
 import { interval } from 'rxjs';
 import { JobofferService } from 'src/app/services/joboffer.service';
 import { FilterPipe } from 'src/app/filter.pipe';
+import {ApiDataService} from "../../services/api-data.service";
 
 
 
@@ -19,16 +20,19 @@ export class JobofferComponent implements OnInit {
   locationFilter!: string;
   salaryFilter!: string;
   filters: any = {};
+  data: any[] = [];
 
-
-  constructor(private jobofferService: JobofferService) { }
+  constructor(private jobofferService: JobofferService, private dataService: ApiDataService) { }
 
   ngOnInit(): void {
-    interval(5000).subscribe(() => this.jobofferService
-      .getJobOffer()
-      .subscribe((result: JobOffer[]) => (this.joboffers = result)));
-
-
+    // interval(5000).subscribe(() => this.jobofferService
+    //   .getJobOffer()
+    //   .subscribe((result: JobOffer[]) => (this.joboffers = result)));
+    console.log(this.data);
+    this.dataService.currentData.subscribe(data => {
+      this.data = data;
+      console.log(data, '1')
+    });
   }
   updateFilters(filters: {domain: string, location: string, salary: string}): void {
     this.domainFilter = filters.domain;
--- VaBucherFront/src/app/components/search/search.component.ts
+++ VaBucherFront/src/app/components/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { JobOffer } from 'src/app/models/joboffer';
 import { JobofferService } from 'src/app/services/joboffer.service';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {ApiDataService} from "../../services/api-data.service";
 
 
 @Component({
@@ -12,16 +14,34 @@ export class SearchComponent {
   domainFilter!: string;
   locationFilter!: string;
   salaryFilter!: string;
+  apiData: any[] = [];
+
   @Output() jobOffersUpdated = new EventEmitter<JobOffer[]>();
   @Output() filtersChanged = new EventEmitter<{domain: string, location: string, salary: string}>();
+  private token = 'g2pujecR_5E4LYWFACdOH1oIT58';
 
-  constructor(private jobofferService: JobofferService) { }
+  constructor(private jobofferService: JobofferService,
+              private http: HttpClient,
+              private dataService: ApiDataService) { }
+
   onSubmit() {
+    const apiUrl =  `https://api.pole-emploi.io/partenaire/offresdemploi/v2/offres/search?qualification=0&motsCles=${this.domainFilter}&commune=${this.locationFilter}&origineOffre=0`;
     const joboffer = new JobOffer();
     joboffer.domain = this.domainFilter;
     this.jobofferService.createJobOffer(joboffer).subscribe((result: JobOffer[]) => this.jobOffersUpdated.emit(result));
     this.filterJobOffers();
-
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + this.token,
+    });
+    // @ts-ignore
+    this.http.get(`${apiUrl}`, { headers }).subscribe(({resultats}) => {
+      resultats.forEach((item: any) => {
+        if (!this.apiData.find(x => x.id === item.id)) {
+          this.apiData.push(item);
+          this.dataService.updateData(this.apiData);
+        }
+      });
+    });
   }
 
   filterJobOffers() {
