27fb37f018c15aa1d205bd2adb7fc9590c898dd3
Florian Lejosne <florian.lejosne@ynov.com>
2023-01-26 11:55:34 +0100

    filter working + fix scss

--- VaBucherFront/src/app/components/joboffer/joboffer.component.ts
+++ VaBucherFront/src/app/components/joboffer/joboffer.component.ts
@@ -3,17 +3,22 @@ import { JobOffer } from 'src/app/models/joboffer';
 import { EventEmitter, Input, Output } from '@angular/core';
 import { interval } from 'rxjs';
 import { JobofferService } from 'src/app/services/joboffer.service';
+import { FilterPipe } from 'src/app/filter.pipe';
+
+
 
 @Component({
   selector: 'app-joboffer',
   templateUrl: './joboffer.component.html',
-  styleUrls: ['./joboffer.component.scss']
+  styleUrls: ['./joboffer.component.scss'],
+  providers: [FilterPipe]
 })
 export class JobofferComponent implements OnInit {
   joboffers: JobOffer[] = [];
   domainFilter!: string;
   locationFilter!: string;
   salaryFilter!: string;
+  filters: any = {};
 
 
   constructor(private jobofferService: JobofferService) { }
@@ -24,6 +29,11 @@ export class JobofferComponent implements OnInit {
       .subscribe((result: JobOffer[]) => (this.joboffers = result)));
 
 
+  }
+  updateFilters(filters: {domain: string, location: string, salary: string}): void {
+    this.domainFilter = filters.domain;
+    this.locationFilter = filters.location;
+    this.salaryFilter = filters.salary;
   }
 
 }
--- VaBucherFront/src/app/filter.pipe.ts
+++ VaBucherFront/src/app/filter.pipe.ts
@@ -1,28 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { JobOffer } from 'src/app/models/joboffer';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(joboffers: any[], domainFilter: string, locationFilter: string, salaryFilter: string): any[] {
-    if (!joboffers) {
-      return [];
-    }
-    if (!domainFilter && !locationFilter && !salaryFilter) {
-      return joboffers;
-    }
+
+  transform(joboffers: JobOffer[], domainFilter: string, locationFilter: string, salaryFilter: string): JobOffer[] {
+    if (!joboffers) { return []; }
+    if (!domainFilter && !locationFilter && !salaryFilter) { return joboffers; }
     return joboffers.filter(joboffer => {
-      if (domainFilter && joboffer.title.toLowerCase().indexOf(domainFilter.toLowerCase()) === -1) {
+      if (domainFilter && joboffer.domain.toLowerCase().indexOf(domainFilter.toLowerCase()) === -1) {
         return false;
       }
-      if (locationFilter && joboffer.location.toLowerCase().indexOf(locationFilter.toLowerCase()) === -1) {
+      if (locationFilter && joboffer.localisation.toLowerCase().indexOf(locationFilter.toLowerCase()) === -1) {
         return false;
       }
       if (salaryFilter && joboffer.salaire.toLowerCase().indexOf(salaryFilter.toLowerCase()) === -1) {
         return false;
       }
-      console.log(joboffer);
-      
       return true;
     });
   }
