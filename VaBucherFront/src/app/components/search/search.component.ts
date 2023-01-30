26f0134548cc7f18b1811b689ba0b4f150bb5cb5
FLEJOSNE <florian.lejosne@ynov.com>
2023-01-30 13:14:43 +0100

    [ADD] jobtypes setup

--- VaBucherFront/src/app/components/search/search.component.ts
+++ VaBucherFront/src/app/components/search/search.component.ts
@@ -51,11 +51,7 @@ export class SearchComponent {
   onSubmit() {
     const apiUrl =  `https://api.pole-emploi.io/partenaire/offresdemploi/v2/offres/search?qualification=0&motsCles=${this.domainFilter}&commune=${this.locationFilter}&origineOffre=0`;
     const joboffer = new JobOffer();
-    if (this.domainFilter){
-      joboffer.domain = this.domainFilter
-    } else {
-      joboffer.domain = this.domainFilter.charAt(0).toUpperCase() + this.domainFilter.slice(1);
-    }    
+      joboffer.domain = this.domainFilter.charAt(0).toUpperCase() + this.domainFilter.slice(1);  
     this.jobofferService.createJobOffer(joboffer).subscribe((result: JobOffer[]) => this.jobOffersUpdated.emit(result));
     this.filterJobOffers();
     const headers = new HttpHeaders({
